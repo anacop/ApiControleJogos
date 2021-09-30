b2d25fbb66cbd49f79b9eb7473013f5c3c006884
Ana Clara <ana.claraop@outlook.com>
2021-09-30 01:04:43 -0300

    criando catálogo de jogos usando boas práticas de arquitetura com.NET

---
+++ ApiControleJogos/Services/IJogoService.cs
@@ -0,0 +1,19 @@
+﻿using ApiControleJogos.InputModel;
+using ApiControleJogos.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiControleJogos.Services
+{
+    public interface IJogoService
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+        Task<JogoViewModel> Obter(Guid id);
+        Task<JogoViewModel> Inserir(JogoInputModel jogo);
+        Task Atualizar(Guid id, JogoInputModel jogo);
+        Task Atualizar(Guid id, double preco);
+        Task Remover(Guid id);
+    }
+}
---
+++ ApiControleJogos/Services/JogoService.cs
@@ -0,0 +1,12 @@
+﻿using ApiControleJogos.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiControleJogos.Services
+{
+    public class JogoService
+    {
+    }
+}
